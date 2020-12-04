8d048ef6f3ee94b38ab582bfb364fb3df0539fc4
Miguel Angel Jaimes <66225919+MrFoxxz@users.noreply.github.com>
2020-12-03 21:20:20 -0500

    Updates

---
+++ precache-manifest.3726684ff3aaf0b2a1ddcc7c640df2a8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4febca65a096861296cf4843df01782e",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "388c95e21f87752a7dfd",
+    "url": "/Portfolio/static/css/main.be71726e.chunk.css"
+  },
+  {
+    "revision": "a101d3bb5f3accd2b285",
+    "url": "/Portfolio/static/js/2.c0a0b2c1.chunk.js"
+  },
+  {
+    "revision": "3c73c585782ac05880c0f89bcfdbba5a",
+    "url": "/Portfolio/static/js/2.c0a0b2c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "388c95e21f87752a7dfd",
+    "url": "/Portfolio/static/js/main.35e932e4.chunk.js"
+  },
+  {
+    "revision": "7cf3094fbeb3a2a4da63",
+    "url": "/Portfolio/static/js/runtime-main.15c37c0f.js"
+  }
+]);
